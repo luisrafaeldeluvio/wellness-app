3360dc25b6c21f3071a9320a09a194bf826ed508
lrrd <github@lrrd.anonaddy.com>
2025-11-14 21:18:12 +0800

    Refactor NavBar component to handle active state via onClick prop and improve item rendering

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,44 +1,58 @@
 import tempIcon from "./temp_icon.svg";
+import { useState } from "react";
 
 interface NavItemProps {
   label: string;
   iconURL: string;
   isActive?: boolean;
+  onClick?: any;
 }
 
-export const NavItem = ({ label, iconURL, isActive = false }: NavItemProps) => {
+export const NavItem = ({
+  label,
+  iconURL,
+  isActive = false,
+  onClick,
+}: NavItemProps) => {
   const activeNavItem: string = isActive
     ? "relative before:absolute before:-inset-2 before:-inset-x-6 before:rounded-4xl before:border-1"
-    : ""; //use a state not a prop
+    : "";
+
   return (
-    <div className="flex flex-col items-center justify-center p-4">
+    <li
+      onClick={() => onClick(label)}
+      className="flex flex-col items-center justify-center p-4"
+    >
       <div className={activeNavItem + " mb-2"}>
         <img src={iconURL} />
       </div>
       <span>{label}</span>
-    </div>
+    </li>
   );
 };
 
 const NavItems: NavItemProps[] = [
-  { label: "Home", iconURL: tempIcon, isActive: true },
+  { label: "Home", iconURL: tempIcon },
   { label: "Log", iconURL: tempIcon },
   { label: "Report", iconURL: tempIcon },
   { label: "Menu", iconURL: tempIcon },
 ];
 
 export const NavBar = () => {
+  const [activeTab, setActiveTab] = useState("Home");
+
+  const handleItemClick = (tabName: string) => setActiveTab(tabName);
+
   return (
     <nav className="fixed bottom-0 left-0 w-full border-t border-t-black">
       <ul className="flex justify-around">
         {NavItems.map((item) => (
-          <li>
-            <NavItem
-              label={item.label}
-              iconURL={item.iconURL}
-              isActive={item.isActive}
-            ></NavItem>
-          </li>
+          <NavItem
+            label={item.label}
+            iconURL={item.iconURL}
+            isActive={activeTab === item.label}
+            onClick={handleItemClick}
+          ></NavItem>
         ))}
       </ul>
     </nav>
