698f6987bdc955509095ba70fc0d06e61cab0cbf
lrrd <github@lrrd.anonaddy.com>
2025-12-01 18:15:44 +0800

    Refactor Log component to improve date handling and dynamically display food logs

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,29 +1,57 @@
-import dayjs from "dayjs";
+import dayjs, { Dayjs } from "dayjs";
 import CalendarHead from "../components/Calendar";
 import LogFoodButton from "../components/LogFoodButton";
 import MealGroup from "../components/MealGroup";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { IFoodLog } from "../db/models/foodLog";
 import FoodItem from "../components/FoodItem";
+import { Journal } from "../db";
+import { getJournalByDate } from "../services/journalService";
+import { addFoodLog, getFoodLog } from "../services/foodLogService";
 
-const foodsample: IFoodLog = {
-  id: 123,
-  date: 456,
-  time: 789,
-  mealType: "breakfast",
-  name: "RIce",
-  calorie: 500,
-};
+async function init() {
+  // await newJournal(new Journal(dayjs().toISOString()));
+  const x = await getJournalByDate(dayjs().format("YYYY-MM-DD"));
+  if (x === undefined) return;
+  await addFoodLog(x, {
+    date: dayjs().format("YYYY-MM-DD"),
+    mealType: "breakfast",
+    name: "Egg",
+    calorie: 120,
+  });
+}
+
+// init();
 
 const Log = () => {
-  const [date, setDate] = useState(dayjs());
+  const [date, setDate] = useState<Dayjs>(dayjs());
+  const [data, setData] = useState<Journal | undefined>(undefined);
+  const [foodLogs, setFoodLogs] = useState<IFoodLog[]>([]);
+
+  useEffect(() => {
+    (async () => {
+      const x = await getJournalByDate(date.format("YYYY-MM-DD"));
+      setFoodLogs([]);
+
+      if (x) {
+        const logs = await Promise.all(
+          x.foodLogIDs.map((id) => getFoodLog(id)),
+        );
+        if (logs)
+          setFoodLogs(logs.filter((log): log is IFoodLog => log !== undefined));
+      }
+
+      setData(x);
+    })();
+  }, [date]);
+
   return (
     <>
       <CalendarHead date={date} setDate={setDate}></CalendarHead>
       <MealGroup title="Breakfast" totalKcal={100}>
-        <FoodItem data={foodsample}></FoodItem>
-        <FoodItem data={foodsample}></FoodItem>
-        <FoodItem data={foodsample}></FoodItem>
+        {foodLogs.map((food) => {
+          return <FoodItem data={food}></FoodItem>;
+        })}
       </MealGroup>
       <LogFoodButton></LogFoodButton>
     </>
