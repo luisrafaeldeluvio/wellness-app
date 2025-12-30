b887ca9d07d29c2b477dea77ea8b07eb17c414fd
lrrd <github@lrrd.anonaddy.com>
2025-12-30 19:32:27 +0800

    Update LogFoodButton link and enhance LogFood component comments

--- src/pages/LogFood.tsx
+++ src/pages/LogFood.tsx
@@ -1,5 +1,9 @@
 const LogFood = () => (
-    <p>Log food here</p>
-)
+  <p>Log food here</p>
+  //input food name'
+  // input date (defualt is the date today)
+  //input calories
+  // maybe create a branch with no style yet. YES DO IT. dont focus on the style yet
+);
 
-export default LogFood
+export default LogFood;
