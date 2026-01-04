fde4cdef78018251502d2c511ac5375dda1ee885
lrrd <github@lrrd.anonaddy.com>
2026-01-04 13:15:13 +0800

    Add Profile page and update Menu component to include Profile button

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,8 +1,12 @@
+import Button from "../components/Button";
 import Header from "../components/Header";
 
 const Menu = () => (
   <>
     <Header>Menu</Header>
+    <div>
+      <Button>Profile</Button>
+    </div>
   </>
 );
 export default Menu;
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,8 @@
+import Header from "../components/Header";
+
+const Profile = () => (
+  <>
+    <Header>Profile</Header>
+  </>
+);
+export default Profile;
