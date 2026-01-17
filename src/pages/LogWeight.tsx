d1493694e941546c81fd6192e59507b508dfed0e
lrrd <github@lrrd.anonaddy.com>
2026-01-18 07:45:20 +0800

    Add style to LogWeight page

--- src/pages/LogWeight.tsx
+++ src/pages/LogWeight.tsx
@@ -3,6 +3,8 @@ import { useLocation } from "wouter";
 import Header from "../components/Header";
 import { addWeight, getWeight } from "../services/weightService";
 import { useEffect, useState } from "react";
+import Button from "../components/Button";
+import checkIcon from "../assets/icons/check_circle_24dp_000000_FILL0_wght200_GRAD0_opsz24.svg";
 
 const LogWeight = () => {
   const [, setLocation] = useLocation();
@@ -30,30 +32,46 @@ const LogWeight = () => {
 
   return (
     <>
-      <Header>Log Food</Header>
-      <form action={logWeightFromForm} name="logWeight">
+      <div className="flex flex-row items-center">
+        <Button onClick={() => setLocation("/journal")} style="mr-0">
+          Back
+        </Button>
+        <Header>Log Weight</Header>
+      </div>
+
+      <form action={logWeightFromForm} name="logWeight" className="m-4">
         <fieldset>
-          <legend>Log Weight</legend>
+          <div className="my-8 mt-0! flex flex-row justify-between">
+            <label htmlFor="weightDate">Date</label>
+            <input
+              type="date"
+              name="weightDate"
+              id="weightDate"
+              defaultValue={dayjs().format("YYYY-MM-DD")}
+              required
+            />
+          </div>
 
-          <label htmlFor="weightDate">Date</label>
-          <input
-            type="date"
-            name="weightDate"
-            id="weightDate"
-            defaultValue={dayjs().format("YYYY-MM-DD")}
-            required
-          />
+          <div className="my-8 mt-0! flex flex-row justify-between">
+            <label htmlFor="weight">Weight</label>
+            <input
+              type="number"
+              name="weight"
+              id="weight"
+              defaultValue={currentWeight}
+              required
+              className="text-right"
+            />
+            kg
+          </div>
 
-          <label htmlFor="weight">Weight</label>
-          <input
-            type="number"
-            name="weight"
-            id="weight"
-            defaultValue={currentWeight}
-            required
-          />
-
-          <input type="submit" value="Add" />
+          <Button style="absolute right-0 bottom-0 m-4 rounded-xl border bg-white p-2">
+            <input
+              type="image"
+              className="flex size-9 items-center justify-center"
+              src={checkIcon}
+            />
+          </Button>
         </fieldset>
       </form>
     </>
