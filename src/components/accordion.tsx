3027441d0de97d3ded0ace898fb57bcc62548457
lrrd <github@lrrd.anonaddy.com>
2026-01-18 15:18:51 +0800

    Add an accordion component

---
+++ src/components/accordion.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+interface accordionProp {
+  text: string;
+}
+
+const Accordion = ({
+  text,
+  children,
+}: React.PropsWithChildren<accordionProp>) => {
+  const [show, setshow] = useState<boolean>(false);
+
+  return (
+    <div className={show ? "rounded-4xl border p-4" : ""}>
+      <button
+        className={
+          "flex w-full flex-row items-center justify-between " +
+          (show ? "" : "rounded-4xl border p-4")
+        }
+        type="button"
+        onClick={() => {
+          setshow(!show);
+        }}
+      >
+        <p>{text}</p>
+      </button>
+
+      <div
+        className={
+          "grid overflow-hidden transition-[grid-template-rows] duration-300 " +
+          (show ? " grid-rows-[1fr]" : "grid-rows-[0fr]")
+        }
+      >
+        <div className="min-h-0">{show ? children : null}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
