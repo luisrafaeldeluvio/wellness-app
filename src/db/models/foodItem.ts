c5c01d6a96311b26d339211b05bb901808c452d6
lrrd <github@lrrd.anonaddy.com>
2026-01-20 23:14:37 +0800

    Remove meal type from FoodItem

--- src/db/models/foodItem.ts
+++ src/db/models/foodItem.ts
@@ -1,10 +1,6 @@
-const MealTypeArray = ["breakfast", "lunch", "dinner", "snacks"] as const;
-export type TMealType = (typeof MealTypeArray)[number];
-
 export interface IFoodItem {
   id?: number;
   date: string;
-  mealType: TMealType;
   name: string;
   energy: number;
 }
