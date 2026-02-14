021b2daa180c01a8d58b480c6ba016a8acac77ce
lrrd <github@lrrd.anonaddy.com>
2026-02-14 16:18:22 +0800

    feat: add PageHeader component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,5 +1,28 @@
-const Header = ({ children }: React.PropsWithChildren) => {
+import { useLocation } from "wouter";
+import Button from "./Button";
+
+export const Header = ({ children }: React.PropsWithChildren) => {
   return <h1 className="m-4 text-2xl font-bold">{children}</h1>;
 };
 
-export default Header;
+interface PageHeaderProp {
+  headerText: string;
+  location?: string;
+}
+
+export const PageHeader = ({ headerText, location }: PageHeaderProp) => {
+  const [, setLocation] = useLocation();
+  return (
+    <div className="flex flex-row items-center">
+      <Button
+        onClick={() =>
+          location ? setLocation(location) : window.history.back()
+        }
+        style="mr-0"
+      >
+        Back
+      </Button>
+      <Header>{headerText}</Header>
+    </div>
+  );
+};
